36b750d0d2b77203769f371618f4a71c369bb966
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-10-01 22:39:55 -0700

    Third function completed.

--- PoetrySlam.py
+++ PoetrySlam.py
@@ -17,3 +17,19 @@ def lines_printed_backwards():
 
 
 lines_printed_backwards()
+
+import random
+
+def lines_printed_random():
+    filename = open("E.E.Cummings.txt", "r")
+    lines_list = filename.readlines()
+    random_poem = random.sample(lines_list, len(lines_list))
+    for x in random_poem:
+        print(x)
+    filename.close()
+
+lines_printed_random()
+
+
+
+
