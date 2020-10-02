d7679be9640bbb7b66c943196323ffffe98bbdf4
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-10-01 20:42:35 -0700

    Completed reverse function.

--- PoetrySlam.py
+++ PoetrySlam.py
@@ -1,11 +1,19 @@
 def get_file_lines():
     filename = open("E.E.Cummings.txt", "r")
     content_list = filename.readlines()
+    for i in content_list:
+        print(i)
     filename.close()
-    print(content_list)
     
-
 get_file_lines()
 
 
+def lines_printed_backwards():
+    filename = open("E.E.Cummings.txt", "r")
+    content_list = filename.readlines()
+    for i in reversed(content_list):
+        print(i)
+    filename.close()
 
+
+lines_printed_backwards()
